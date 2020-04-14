d2a70a0ed2ef31786e9cfcc1f2eb7743aa9e3703
Gaffinator <gaffney@oakland.edu>
2020-04-14 13:55:30 -0400

    added globals
    globals

--- app/src/main/java/com/example/ollux/cryptit/Globals.java
+++ app/src/main/java/com/example/ollux/cryptit/Globals.java
@@ -4,11 +4,14 @@ package com.example.ollux.cryptit;
 import android.app.Application;
 
 import java.security.KeyPair;
+import java.security.PublicKey;
 
 public class Globals extends Application {
     public RSA myRSA= new RSA();
     public USER currUser= new USER();
-
+    public int  numOfFriends;
+    public PublicKey[] publicKeys = new PublicKey[25];
+    public String[] friendNames = new String [25];
 
     public USER getUser(){
         return this.currUser;
